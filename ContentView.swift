7dcb46e7bef9a940ba847053596d1c69ff51607d
alfo0924 <146694784+alfo0924@users.noreply.github.com>
2024-11-26 09:32:46 +0800

    Update ContentView.swift

--- ContentView.swift
+++ ContentView.swift
@@ -1,15 +1,26 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var counter1 = 0
-    @State private var counter2 = 0
-    @State private var counter3 = 0
+    @AppStorage("counter1") private var counter1 = 0
+    @AppStorage("counter2") private var counter2 = 0
+    @AppStorage("counter3") private var counter3 = 0
     
     var body: some View {
         VStack {
-            CounterButton(counter: $counter1, color: .green)
-            CounterButton(counter: $counter2, color: .blue)
-            CounterButton(counter: $counter3, color: .red)
+            CounterButton(counter: Binding(
+                get: { counter1 },
+                set: { counter1 = $0 }
+            ), color: .green)
+            
+            CounterButton(counter: Binding(
+                get: { counter2 },
+                set: { counter2 = $0 }
+            ), color: .blue)
+            
+            CounterButton(counter: Binding(
+                get: { counter3 },
+                set: { counter3 = $0 }
+            ), color: .red)
         }
     }
 }
